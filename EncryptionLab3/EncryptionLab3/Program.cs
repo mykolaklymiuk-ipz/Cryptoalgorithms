50d8738ff04c6fd100143d05ccfd43939f285aa8
mykolaklymiuk-ipz <mykola.klymiuk.22@pnu.edu.ua>
2022-12-28 15:18:43 +0200

    Lab 3

---
+++ EncryptionLab3/EncryptionLab3/Program.cs
@@ -0,0 +1,15 @@
+﻿using EncryptionLab3;
+using System;
+
+Console.Write("Input encryption key: ");
+var key = Console.ReadLine();
+
+Console.Write("Input message to encrypt: ");
+var message = Console.ReadLine();
+
+var encryptedMessage = DesProvider.Encrypt(message, key);
+Console.WriteLine($"Encrypted message: {Convert.ToHexString(encryptedMessage)}");
+
+var decryptedMessage = DesProvider.Decrypt(encryptedMessage, key);
+Console.WriteLine($"Decrypted message: {decryptedMessage}");
+
